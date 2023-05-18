243c94cb497a9a5d0dfc0b02d68c39fb0b33d9ff
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-05-18 15:08:24 +0300

    wip

--- Ignite.DynamicLINQ/Program.cs
+++ Ignite.DynamicLINQ/Program.cs
@@ -1,6 +1,12 @@
+using Ignite.DynamicLINQ.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton(new IgniteService());
+builder.Services.AddScoped<CarRepository>();
+
 var app = builder.Build();
 
+// TODO: Add swagger and open the API page on start?
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
