aa630c88a78df6fb65a43cb57f5b8ff3a220139f
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-05-18 20:07:34 +0300

    add SearchMode

---
+++ Ignite.DynamicLINQ/Data/SearchMode.cs
@@ -0,0 +1,8 @@
+namespace Ignite.DynamicLINQ.Data;
+
+public enum SearchMode
+{
+    None,
+    Any,
+    All
+}
--- Ignite.DynamicLINQ/Program.cs
+++ Ignite.DynamicLINQ/Program.cs
@@ -1,4 +1,5 @@
 using Ignite.DynamicLINQ.Data;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton(new IgniteService());
@@ -8,5 +9,13 @@ var app = builder.Build();
 
 // TODO: Add swagger and open the API page on start?
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet(
+        "/cars",
+        (string? make,
+                string? model,
+                int? year,
+                SearchMode? searchMode,
+                [FromServices] CarRepository repo) => repo.GetCars()); // TODO
 
 app.Run();
