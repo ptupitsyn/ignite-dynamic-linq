c51f684364a11f9a3e859f4c629f7ba289bfa579
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-05-19 08:14:33 +0300

    wip tests

---
+++ Ignite.DynamicLINQ.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Ignite.DynamicLINQ.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
