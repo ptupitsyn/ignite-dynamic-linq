8583410e43ec84a24f1d52ee9da5d8e3c9fafee6
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-05-22 10:35:14 +0300

    Add benchmark results

--- Ignite.DynamicLINQ.Benchmarks/Program.cs
+++ Ignite.DynamicLINQ.Benchmarks/Program.cs
@@ -4,6 +4,16 @@ using Ignite.DynamicLINQ.Data;
 
 BenchmarkRunner.Run<CarRepositoryBenchmark>();
 
+/// <summary>
+/// |      Method | SearchMode |       Mean |     Error |    StdDev |   Gen0 |   Gen1 | Allocated |
+/// |------------ |----------- |-----------:|----------:|----------:|-------:|-------:|----------:|
+/// |        Linq |        Any |  45.740 us | 0.2229 us | 0.1976 us | 2.3193 | 0.1221 |  28.46 KB |
+/// | LinqDynamic |        Any |  81.645 us | 0.4482 us | 0.3500 us | 4.8828 | 0.2441 |  61.12 KB |
+/// |         Sql |        Any |   7.035 us | 0.0492 us | 0.0460 us | 0.1984 | 0.0916 |   2.45 KB |
+/// |        Linq |        All | 337.704 us | 2.6861 us | 2.3812 us | 3.9063 | 2.9297 |  53.56 KB |
+/// | LinqDynamic |        All |  80.868 us | 0.3346 us | 0.2794 us | 4.8828 | 0.2441 |  61.13 KB |
+/// |         Sql |        All |   6.969 us | 0.0398 us | 0.0353 us | 0.1984 | 0.0916 |   2.46 KB |
+/// </summary>
 [MemoryDiagnoser]
 public class CarRepositoryBenchmark
 {
